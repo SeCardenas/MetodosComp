90b5b3c90c8fc393f1c11f47ee520cf73a6048ff
SeCardenas <30929199+SeCardenas@users.noreply.github.com>
2018-09-07 15:44:12 -0500

    avances primer punto

--- CardenasSergio_hw1/CardenasSergio_temp.py
+++ CardenasSergio_hw1/CardenasSergio_temp.py
@@ -6,6 +6,13 @@ anoms = np.array([])
 anoms2 = np.array([])
 for line in file:
     a = line.split("\t")
-    years = np.append(years, [a[0]])
-    anoms = np.append(anoms, [a[1]])
-    anoms2 = np.append(anoms2, [a[2]])
+    years = np.append(years, int(a[0]))
+    anoms = np.append(anoms, float(a[1]))
+    anoms2 = np.append(anoms2, float(a[2]))
+plt.plot(years, anoms)
+plt.title("Anomalias de temperatura")
+plt.savefig("Grafica_temp.pdf")
+
+indexes = np.where(anoms>0.5)
+print "anomalias mayores a 0.5:"
+print years[indexes]
