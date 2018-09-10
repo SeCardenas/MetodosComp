1c9a01f5f10b102cb85cc0ec2af1627f9881b404
SeCardenas <30929199+SeCardenas@users.noreply.github.com>
2018-09-10 14:58:18 -0500

    Update CardenasSergio_temp.sh

--- CardenasSergio_hw1/CardenasSergio_temp.sh
+++ CardenasSergio_hw1/CardenasSergio_temp.sh
@@ -1,11 +1,11 @@
 mkdir Temperaturas
 wget https://climate.nasa.gov/system/internal_resources/details/original/647_Global_Temperature_Data_File.txt 
 cd Temperaturas
-cp ~/tempdata.txt ./tempdata.txt
-awk '{for(i = 2; i <= 13; i++) {if($i < -30.0 && $i != "") {print $1; break;}}}' tempdata.txt? > menorque30.txt
-awk '{a = 1; for(i = 2; i <= 13; i++) {if($i > -6.0 || $i == "") {a = 0; break;}} if(a) {print $1}}' tempdata.txt? > menorque6.txt
-awk '{if($7 != "J" && $7 != "") print $7;}' tempdata.txt? > junio.txt
-#creo el archivo para poder imprimir el numero.
-awk 'BEGIN {a = 0}{if($14 > -15.0) a++;} END {print a}' tempdata.txt
-rm tempdata.txt
-#python CardenasSergio_temp.py
+cp ~/tempdata_masfacil.txt ./tempdata_masfacil.txt
+awk 'BEGIN {FS=","} {for(i = 2; i <= 13; i++) {if($i < -30.0 && $i != "" && $1 != "    ") {print $1; break;}}}' tempdata_masfacil.txt? > menorque30.txt
+awk 'BEGIN {FS=","} {a = 1; for(i = 2; i <= 13; i++) {if($i > -6.0 || $i == "") {a = 0; break;}} if(a && $1 != "    ") {print $1}}' tempdata_masfacil.txt? > menorque6.txt
+awk 'BEGIN {FS=","} {if($7 != "  J  " && $7 != "") print $7;}' tempdata_masfacil.txt? > junio.txt
+awk 'BEGIN {FS=","; a = 0}{if($14 > -15.0) a++;} END {print a}' tempdata_masfacil.txt
+rm tempdata_masfacil.txt
+cd ~/ 
+python CardenasSergio_temp.py 
